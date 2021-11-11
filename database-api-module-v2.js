0f9e8452fe5ca5ce4a49a8e3e91ed0d96cdcd9c4
hassan1091 <roh.baba.11@gmail.com>
2021-11-11 18:38:47 +0300

    uninstall and delete node-schedule

--- database-api-module-v2.js
+++ database-api-module-v2.js
@@ -1,37 +1,7 @@
-const https = require("https");
 const axios = require("axios"); // using for post request
-const schedule = require('node-schedule'); // for schedule the event
 
 let AuthorizationToken = "Bearer G-xyDeLzMjbBSbTzIwmFIxyAx2mJDt1n-XFFhURid8sqeGBjGe-JzyZFj5X7EnJbYoYPo17hPj9WfPPi7mvZfgjMk2i0mNQ4iJ2UA5BTDzxiC4bmT8opFXgIZfZKqIuCBNvStPs_tQL750EIk-Z10mLbq6vgm5AzyU6HM_rY7F5m09Um1preUMWCX1oBSD9Jp0oKueN_sXjsoedKgeSskrSY8irAZExhD8knkT31B4f_lLk6gx35BGaKMsQi5Aiubm6tp0j01ELXg1HUTRcwEGWFdWFILcpcfhPuWOFAdIW5sCLOLH0Qu4tBY33eMhZFSz6suIsOBl8ra1gDKfuSLdBF4egwEbB4bPt-HZoAtvSa9ONtjJ8n37422Lxnzxqi";
 
-
-
-/*const rule = new schedule.RecurrenceRule();
-rule.hour = 0;
-rule.tz = "Asia/Riyadh";
-
-const job = schedule.scheduleJob(rule, () => {}
-  
- let data =" grant_type=client_credentials&client_id=da734859e9554e6579f7aa274945e625dcdf640018821f2f01&client_secret=54ce51753685479eb44f91cf99f1ca8ba9983465cb9b3b84cd";
-
-  */
-
-  /*let config = {
-    method: "post",
-    url: "https://c2aco568.caspio.com/oauth/token",
-    headers: {
-      "Content-Type": "text/plain",
-    },
-    data: data,
-  };
-axios(config)
-    .then((response) => {
-      AuthorizationToken = "Bearer " + response.data.access_token;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-*/
 exports.getAdmins =  (callBack)=>{
     const AllAdminsApi ="https://c2aco568.caspio.com/rest/v2/tables/admins/records";
     var config = {
