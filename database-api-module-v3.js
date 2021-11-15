396508513c43c24396a630be418b0bd5e9b731c1
hassan1091 <roh.baba.11@gmail.com>
2021-11-15 14:03:19 +0300

    setup the v3 with update member by id

--- database-api-module-v3.js
+++ database-api-module-v3.js
@@ -67,6 +67,22 @@ exports.creatMember = (newMemeber, callBack) => {
     });
 };
 
-exports.updateMember = (id, type, newValue, callBack) => {};
+exports.updateMember = (id, type, newValue, callBack) => {
+  let updatedValue 
+  if (type === "Name") {
+    updatedValue = {"name": newValue,};
+  } else if (type === "Warnings") {
+    updatedValue = {"warnings": newValue,};
+  } else {
+    updatedValue = {"money": newValue,};
+  }
+  MemberModel.findByIdAndUpdate(id,{$set: updatedValue},(error, result) => {
+    if (error){
+      console.log(error);
+      callBack(null);
+    } 
+    else callBack(result);
+  });
+};
 
 exports.deleteMember = (id, callBack) => {};
